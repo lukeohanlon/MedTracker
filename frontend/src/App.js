e2170558ad50c9d81e03e36b22bede8774b951a9
luke0han <lukeohanlon@outlook.com>
2023-08-10 19:39:50 +0100

    links routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import './index.css'
 import MedicationList from './components/MedicationList'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MyMeds from './views/MyMeds'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="App">
         <Routes>
           <Route path="/" exact element={<MedicationList />} />
