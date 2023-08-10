9246fa7f1afd2ccf234d09a6935b0a301fe1d2dc
luke0han <lukeohanlon@outlook.com>
2023-08-10 23:37:21 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# Define environment variables
-export RAILS_ENV=production
+# Stop and remove the existing frontend container
+FRONTEND_CONTAINER=$(docker ps -a -q --filter ancestor="$FRONTEND_IMAGE" --format="{{.ID}}")
+if [ "$FRONTEND_CONTAINER" ]; then
+  docker rm -f "$FRONTEND_CONTAINER"
+fi
 
-# Navigate to the application directory
-cd /app
+# Stop and remove the existing backend container
+BACKEND_CONTAINER=$(docker ps -a -q --filter ancestor="$BACKEND_IMAGE" --format="{{.ID}}")
+if [ "$BACKEND_CONTAINER" ]; then
+  docker rm -f "$BACKEND_CONTAINER"
+fi
 
-# Bring down any existing containers
-docker-compose down
+# Pull the latest frontend image from the container registry
+docker pull "$FRONTEND_IMAGE"
 
-# Pull the latest images
-docker-compose pull
+# Pull the latest backend image from the container registry
+docker pull "$BACKEND_IMAGE"
 
-# Start the containers in detached mode
-docker-compose up -d
+# Create and start the frontend container with the specified port mapping
+docker create -p 3001:3001 --name frontend-container "$FRONTEND_IMAGE"
+docker start frontend-container
+
+# Create and start the backend container with the specified port mapping
+docker create -p 3000:3000 --name backend-container "$BACKEND_IMAGE"
+docker start backend-container
+
