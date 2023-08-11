6e7077cf1916c310b56d50b05d67aad598733743
lukeohanlon <126015417+lukeohanlon@users.noreply.github.com>
2023-08-11 03:37:46 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,3 +26,26 @@ docker start frontend-container
 docker create -p 3000:3000 --name backend-container "$BACKEND_IMAGE"
 docker start backend-container
 
+# Update the system
+sudo apt update
+
+# Install RVM
+curl -sSL https://get.rvm.io | bash
+source ~/.rvm/scripts/rvm
+
+# Install Ruby version 3.2.2 and set as default
+rvm install 3.2.2
+rvm use 3.2.2 --default
+
+# Change directory into the application folder
+cd MedTracker/backend
+
+# Run database migrations
+rails db:migrate RAILS_ENV=development
+
+# Pass in certs for HTTPS
+#echo "$PRIVATE_KEY" > privatekey.pem
+#echo "$SERVER_CERT" > server.crt
+
+# Start the Rails server with HTTPS support
+rails server --binding 0.0.0.0 -p 3000
